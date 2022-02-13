feb126254e0c336c91e334933148c9b68719ae5f
ivanfsilva <ivanfs27@gmail.com>
2022-02-13 13:24:32 -0300

    created DTO standard for Tecnico

--- backend/src/main/java/br/com/ivanfsilva/helpdesk/resources/TecnicoResource.java
+++ backend/src/main/java/br/com/ivanfsilva/helpdesk/resources/TecnicoResource.java
@@ -1,6 +1,7 @@
 package br.com.ivanfsilva.helpdesk.resources;
 
 import br.com.ivanfsilva.helpdesk.domain.Tecnico;
+import br.com.ivanfsilva.helpdesk.domain.dtos.TecnicoDTO;
 import br.com.ivanfsilva.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +18,9 @@ public class TecnicoResource {
     private TecnicoService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Tecnico> findById( @PathVariable Integer id ) {
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id ) {
         Tecnico obj = service.findById(id);
 
-        return ResponseEntity.ok().body(obj);
+        return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 }
