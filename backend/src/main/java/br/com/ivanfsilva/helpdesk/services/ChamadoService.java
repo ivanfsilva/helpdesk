5295c397df8c9ea9204c971ad508f03f00097ef6
ivanfsilva <ivanfs27@gmail.com>
2022-02-28 16:02:47 -0300

    CRUD Chamado implemented

---
+++ backend/src/main/java/br/com/ivanfsilva/helpdesk/services/ChamadoService.java
@@ -0,0 +1,71 @@
+package br.com.ivanfsilva.helpdesk.services;
+
+import br.com.ivanfsilva.helpdesk.domain.Chamado;
+import br.com.ivanfsilva.helpdesk.domain.Cliente;
+import br.com.ivanfsilva.helpdesk.domain.Tecnico;
+import br.com.ivanfsilva.helpdesk.domain.dtos.ChamadoDTO;
+import br.com.ivanfsilva.helpdesk.domain.enums.Prioridade;
+import br.com.ivanfsilva.helpdesk.domain.enums.Status;
+import br.com.ivanfsilva.helpdesk.repositories.ChamadoRepository;
+import br.com.ivanfsilva.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository repository;
+    @Autowired
+    private TecnicoService tecnicoService;
+    @Autowired
+    private ClienteService clienteService;
+
+    public Chamado findById(Integer id) {
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+    }
+
+    public List<Chamado> findAll() {
+        return repository.findAll();
+    }
+
+    public Chamado create(ChamadoDTO obj) {
+        return repository.save(newChamado(obj));
+    }
+
+    public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+        objDTO.setId(id);
+        Chamado oldObj = findById(id);
+        oldObj = newChamado(objDTO);
+        return repository.save(oldObj);
+    }
+
+    private Chamado newChamado(ChamadoDTO obj) {
+        Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+        Cliente cliente = clienteService.findById(obj.getCliente());
+
+        Chamado chamado = new Chamado();
+        if(obj.getId() != null) {
+            chamado.setId(obj.getId());
+        }
+
+        if(obj.getStatus().equals(2)) {
+            chamado.setDataFechamento(LocalDate.now());
+        }
+
+        chamado.setTecnico(tecnico);
+        chamado.setCliente(cliente);
+        chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+        chamado.setStatus(Status.toEnum(obj.getStatus()));
+        chamado.setTitulo(obj.getTitulo());
+        chamado.setObservacao(obj.getObservacao());
+        return chamado;
+    }
+
+}
