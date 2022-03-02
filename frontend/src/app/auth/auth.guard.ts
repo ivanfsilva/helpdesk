615b160aecc6af1140efb78648b8a11bc4e65949
ivanfsilva <ivanfs27@gmail.com>
2022-03-02 10:48:11 -0300

    interceptor for HTTP requests

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
-import { Observable } from 'rxjs';
-import {AuthService} from "../services/auth.service";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from "../services/auth.service";
 
 @Injectable({
   providedIn: 'root'
