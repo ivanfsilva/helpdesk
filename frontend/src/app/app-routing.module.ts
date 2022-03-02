45ee638ac63cf2882222332d23fcd5a41ea3c5f5
ivanfsilva <ivanfs27@gmail.com>
2022-03-02 19:01:48 -0300

    route to component chamado-update

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { ClienteUpdateComponent } from "./components/cliente/cliente-update/clie
 import { ClienteListComponent } from "./components/cliente/cliente-list/cliente-list.component";
 import { ChamadoListComponent } from "./components/chamdo/chamado-list/chamado-list.component";
 import { ChamadoCreateComponent } from "./components/chamdo/chamado-create/chamado-create.component";
+import { ChamadoUpdateComponent } from "./components/chamdo/chamado-update/chamado-update.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -32,6 +33,7 @@ const routes: Routes = [
 
       { path: 'chamados', component: ChamadoListComponent },
       { path: 'chamados/create', component: ChamadoCreateComponent },
+      { path: 'chamados/update/:id', component: ChamadoUpdateComponent },
     ]
   }
 ];
