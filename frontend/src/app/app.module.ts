cb537db3c5436a4045cbb1fe378c021ad473b48c
ivanfsilva <ivanfs27@gmail.com>
2022-03-02 17:16:48 -0300

    chamado-list component created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -44,6 +44,7 @@ import {ClienteCreateComponent} from "./components/cliente/cliente-create/client
 import {ClienteUpdateComponent} from "./components/cliente/cliente-update/cliente-update.component";
 import {ClienteDeleteComponent} from "./components/cliente/cliente-delete/cliente-delete.component";
 import {ClienteListComponent} from "./components/cliente/cliente-list/cliente-list.component";
+import { ChamadoListComponent } from './components/chamdo/chamado-list/chamado-list.component';
 
 
 @NgModule({
@@ -60,7 +61,8 @@ import {ClienteListComponent} from "./components/cliente/cliente-list/cliente-li
     ClienteListComponent,
     ClienteCreateComponent,
     ClienteUpdateComponent,
-    ClienteDeleteComponent
+    ClienteDeleteComponent,
+    ChamadoListComponent
   ],
   imports: [
     BrowserModule,
