c6040a79e4105a8d0e05acbec204a4cfa03d91bb
ivanfsilva <ivanfs27@gmail.com>
2022-03-02 23:05:25 -0300

    bug fix "failed to lazily initialize a function collection"

--- backend/src/main/java/br/com/ivanfsilva/helpdesk/domain/Cliente.java
+++ backend/src/main/java/br/com/ivanfsilva/helpdesk/domain/Cliente.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +19,7 @@ public class Cliente extends Pessoa {
     private static final long serialVersionUID = 1L;
 
     @JsonIgnore
-    @OneToMany(mappedBy = "cliente")
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Cliente() {
--- backend/src/main/java/br/com/ivanfsilva/helpdesk/domain/Tecnico.java
+++ backend/src/main/java/br/com/ivanfsilva/helpdesk/domain/Tecnico.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,17 +18,17 @@ public class Tecnico extends Pessoa {
     private static final long serialVersionUID = 1L;
 
     @JsonIgnore
-    @OneToMany(mappedBy = "tecnico")
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
         super();
-        addPerfil(Perfil.CLIENTE);
+        addPerfil(Perfil.TECNICO);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
-        addPerfil(Perfil.CLIENTE);
+        addPerfil(Perfil.TECNICO);
     }
 
     public Tecnico( TecnicoDTO obj ) {
